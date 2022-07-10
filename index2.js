8922789b527987161347a962fa9cd3677a6c73f1
Michael Ruhl <michaelruhl14@hotmail.com>
2022-07-10 18:43:46 +0000

    improved

---
+++ index2.js
@@ -0,0 +1,25 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised')
+
+
+let printPassTimes = function(passTimes) {
+    for (const pass of passTimes) {
+      const datetime = new Date(0);
+      datetime.setUTCSeconds(pass.risetime);
+      const duration = pass.duration;
+      console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+    }
+  };
+
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+
+  .catch((error) => {
+    console.log('not work: ', error.message);
+  });
+
+
+
+  
